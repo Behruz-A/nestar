fb76ce9d310ac29d91819d47cbf87670557a1767
Behruz-A <behruzanorboev1@gmail.com>
2026-08-09 12:40:16 +0900

    fix: connect MongoDB database

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,8 +1,11 @@
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT_API ?? 3000);
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.PORT_API ?? 3000);
 }
 bootstrap();
