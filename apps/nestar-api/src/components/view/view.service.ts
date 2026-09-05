9100ec68c5ab075aa89e82c596bfdf301506ca4b
Behruz-A <behruzanorboev1@gmail.com>
2026-09-05 14:28:01 +0900

    feat: develop getVisited GraphQl api

--- apps/nestar-api/src/components/view/view.service.ts
+++ apps/nestar-api/src/components/view/view.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { View } from '../../libs/dto/view/view';
 import { ViewInput } from '../../libs/dto/view/view.input';
 import { T } from '../../libs/types/common';
+import { Properties } from '../../libs/dto/property/property';
+import { OrdinaryInquiry } from '../../libs/dto/property/property.input';
+import { ViewGroup } from '../../libs/enums/view.enum';
+import { lookupVisit } from '../../libs/config';
 
 @Injectable()
 export class ViewService {
@@ -19,5 +23,53 @@ export class ViewService {
 		const { memberId, viewRefId } = input;
 		const search: T = { memberId: memberId, viewRefId: viewRefId };
 		return await this.viewModel.findOne(search).exec();
+	}
+
+	public async getVisitedProperties(memberId: ObjectId, inqut: OrdinaryInquiry): Promise<Properties> {
+		const { page, limit } = inqut;
+
+		const match: T = { viewGroup: ViewGroup.PROPERTY, memberId: memberId };
+
+		const data: T = await this.viewModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: { updatedAt: -1 } },
+
+				{
+					$lookup: {
+						from: 'properties',
+						localField: 'viewRefId',
+						foreignField: '_id',
+						as: 'visitedProperty',
+					},
+				},
+
+				{
+					$unwind: '$visitedProperty',
+				},
+
+				{
+					$facet: {
+						list: [
+							{ $skip: (page - 1) * limit },
+							{ $limit: limit },
+
+							lookupVisit,
+							{ $unwind: '$visitedProperty.memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+
+		console.log('data', data);
+
+		const result: Properties = { list: [], metaCounter: data[0].metaCounter };
+
+		result.list = data[0].list.map((ele) => ele.visitedProperty);
+		console.log('result', result);
+
+		return result;
 	}
 }
