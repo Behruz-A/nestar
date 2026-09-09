e1db52553199004be09b75b796188d2272dd142a
Behruz-A <behruzanorboev1@gmail.com>
2026-09-09 22:56:24 +0900

    feat: create batchRollback, batchProperties and batchAgents job schedules logic

---
+++ apps/nestar-batch/src/libs/config.ts
@@ -0,0 +1,9 @@
+/**
+ * ************************
+ * BATCH CONSTANS
+ * ***************************
+ */
+
+export const BATCH_TOP_ROLLBACK = 'BATCH_TOP_ROLLBACK';
+export const BATCH_TOP_PROPERTIES = 'BATCH_TOP_PROPERTIES';
+export const BATCH_TOP_AGENTS = 'BATCH_TOP_AGENTS';
