82ebc604e76deebc9d77629502aec750ac7c5da6
Behruz-A <behruzanorboev1@gmail.com>
2026-09-09 23:18:00 +0900

    fix: modify job scheduler business logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
-import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { Cron, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './libs/config';
 
 @Controller()
 export class BatchController {
@@ -8,48 +9,49 @@ export class BatchController {
 	constructor(private readonly batchService: BatchService) {}
 
 	@Timeout(1000)
-	hadleTimeout() {
-		this.logger.debug('BATCH SERVER READY');
+	handleTimeOut() {
+		this.logger.debug('BATCH SERVER READY!');
 	}
 
-	@Cron('00 * * * * *', { name: 'BATCH_ROLLBACK' })
+	@Cron('00 00 01 * * *', { name: BATCH_ROLLBACK })
 	public async batchRollback() {
 		try {
-			this.logger['context'] = 'BATCH_ROLLBACK';
-			this.logger.debug('EXECUTED');
+			this.logger['context'] = BATCH_ROLLBACK;
+			this.logger.debug('EXECUTED!');
 			await this.batchService.batchRollback();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
 
-	@Cron('20  * * * * *', { name: 'BATCH_TOP_PROPERTIES' })
-	public async batchProperties() {
+	@Cron('20 00 01 * * *', { name: BATCH_TOP_PROPERTIES })
+	public async batchTopProperties() {
 		try {
-			this.logger['context'] = 'BATCH_TOP_PROPERTIES';
-			this.logger.debug('EXECUTED');
-			await this.batchService.batchProperties();
+			this.logger['context'] = BATCH_TOP_PROPERTIES;
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopProperties();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
 
-	@Cron('40 * * * * *', { name: 'BATCH_TOP_AGENTS' })
-	public async batchAgents() {
+	@Cron('40 00 01 * * *', { name: BATCH_TOP_AGENTS })
+	public async batchTopAgents() {
 		try {
-			this.logger['context'] = 'BATCH_TOP_AGENTS';
-			this.logger.debug('EXECUTED');
-			await this.batchService.batchAgents();
+			this.logger['context'] = BATCH_TOP_AGENTS;
+			this.logger.debug('EXECUTED!');
+			await this.batchService.batchTopAgents();
 		} catch (err) {
 			this.logger.error(err);
 		}
 	}
 
-	// @Interval(1000)
-	// handleInterval() {
-	// 	this.logger.debug('INTERVAL TEST');
-	// }
-
+	/*
+	@Interval(1000)
+	handleInterval() {
+    this.logger.debug('INTERVAL TEST')
+  }
+  */
 	@Get()
 	getHello(): string {
 		return this.batchService.getHello();
